7f9899c3327a7f7b7f55f467bfea8e808616687e
Salim <salim.ullah@beis.gov.uk>
2018-01-04 11:18:40 +0000

    rem scala buildpack

--- push.sh
+++ push.sh
@@ -2,7 +2,12 @@
 
 echo "setting env variables"
 
-
+export ACCESS_KEY=polar
+export SECRET_KEY=bear
+export BUCKET_NAME=albatross
+export REGION=eu-west-2
+export FILE_DOMAIN=penguin
+export SICE_DUMMYMANAGER=salim.ullah@beis.gov.uk
 
 echo "sbt run.."
 
@@ -10,6 +15,6 @@ sbt stage
 
 echo "cf Push .."
 
-cf push
+cf push ci1 -p ./target/universal/stage -b java_buildpack --health-check-type=none
 
 echo "cf Push complete"
