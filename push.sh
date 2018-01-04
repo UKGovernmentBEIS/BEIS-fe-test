5def9abd80b19bc26f85ba25dd3d1145f06cd1f7
Salim <salim.ullah@beis.gov.uk>
2018-01-04 11:39:01 +0000

    push change

--- push.sh
+++ push.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-echo "setting env variables"
-
-export ACCESS_KEY=polar
-export SECRET_KEY=bear
-export BUCKET_NAME=albatross
-export REGION=eu-west-2
-export FILE_DOMAIN=penguin
-export SICE_DUMMYMANAGER=salim.ullah@beis.gov.uk
-
 echo "sbt run.."
 
 sbt stage
@@ -18,3 +9,4 @@ echo "cf Push .."
 cf push ci1 -p ./target/universal/stage -b java_buildpack --health-check-type=none
 
 echo "cf Push complete"
+echo "code"
